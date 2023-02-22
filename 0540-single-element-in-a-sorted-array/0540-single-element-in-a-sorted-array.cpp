7145d037d7df9af9b3504d3962e43febb678d805
Baccari Ala <93733136+10Ala10@users.noreply.github.com>
2023-02-22 21:54:21 +0100

    Time: 33 ms (33.13%), Space: 22.3 MB (63.14%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,18 @@
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int ans=0;
-        for(auto &x:nums){
-            ans^=x;
+class Solution
+{
+    public:
+        int singleNonDuplicate(vector<int> &nums)
+        {
+            int left = 0;
+            int right = (int)nums.size() - 2;
+            while(left <= right){
+                int mid = (left + right) >> 1;
+                if(nums[mid] == nums[mid^1]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+            return nums[left];
         }
-        return ans;
-    }
 };
