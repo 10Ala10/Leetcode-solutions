ecb30680903abde336c3f67963d1ef8f990eeffd
Baccari Ala <93733136+10Ala10@users.noreply.github.com>
2023-02-24 23:13:16 +0100

    Time: 3 ms (95.32%), Space: 13.7 MB (58.17%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -7,8 +7,7 @@ class Solution
             int prev1, prev2, temp;
             prev1 = 0;
             prev2 = cost[0];
-            for (int i = 2; i <= n; ++i)
-            {
+            for (int i = 2; i <= n; ++i){
                 temp = min(prev1, prev2) + cost[i - 1];
                 prev1 = prev2;
                 prev2 = temp;
