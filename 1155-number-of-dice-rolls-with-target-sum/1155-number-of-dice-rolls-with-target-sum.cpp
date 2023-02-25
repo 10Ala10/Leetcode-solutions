72077d5c458a8e6f70fc00b591ed7c3c9dbec508
Baccari Ala <93733136+10Ala10@users.noreply.github.com>
2023-02-25 15:41:25 +0100

    Time: 38 ms (60.96%), Space: 6.1 MB (89.78%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,19 @@
+const int mod = (int)1e9 + 7;
+class Solution {
+public:
+    int numRollsToTarget(int n, int f, int target) {
+        int dp[n + 1][target + 1];
+        memset(dp,0,sizeof(dp));
+        dp[0][0] = 1;
+        for(int i = 1;i <= n; ++i){
+            for(int j = 1; j <= target; ++j){
+                for(int k = 1; k <= f; ++k){
+                    if(k <= j){
+                    dp[i][j] = ((dp[i][j] % mod) + (dp[i-1][j - k] % mod)) % mod;
+                    }
+                }
+            }
+        }
+        return dp[n][target];
+    }
+};
