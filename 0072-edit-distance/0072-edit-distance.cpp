9b24270a6d6c5760bb6e26cdf845f81afaf4a7eb
Baccari Ala <93733136+10Ala10@users.noreply.github.com>
2023-02-26 07:45:32 +0100

    Time: 6 ms (93.28%), Space: 6.2 MB (99.17%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,25 +3,26 @@ public:
     int minDistance(string word1, string word2) {
         int n = (int)word1.size();
         int m = (int)word2.size();
-        //dp[i][j] is the minimum number of operations to convert word1[0..i) to word2[0..j)
-        int dp[n + 1][m + 1];
+        int prev[m + 1], temp[m + 1];
         //base case
-        for(int i = 0; i <= n; ++i){
-            dp[i][0] = i;
-        }
         for(int j = 0; j <=m; ++j){
-            dp[0][j] = j;
+            prev[j] = j;
         }
 
         for(int i = 1; i <= n; ++i){
+            temp[0] = i; // base case
             for(int j = 1; j <= m; ++j){
                 if(word1[i - 1] == word2[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1];
+                    temp[j] = prev[j - 1];
                 }else{
-                    dp[i][j] = min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]}) + 1;
+                    temp[j] = min({prev[j - 1], temp[j - 1], prev[j]}) + 1;
                 }
             }
+            //swap
+            for(int j = 0; j <= m; ++j){
+                prev[j] = temp[j];
+            }
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
