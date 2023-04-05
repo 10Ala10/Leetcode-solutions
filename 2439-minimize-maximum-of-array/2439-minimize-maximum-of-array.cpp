c52b2e2ec8b36f802606a955327e5befba62f828
Baccari Ala <93733136+10Ala10@users.noreply.github.com>
2023-04-05 20:08:24 +0100

    Time: 146 ms (63.04%), Space: 71.4 MB (61.54%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     typedef long long ll;
-
-    // Function to check if a given target value is valid
-    // by checking if it's possible to make all elements of nums <= target
     bool isValid(ll target, vector<int>& nums){
-        if(nums[0]>target) return false; 
-        ll prev=nums[0];
-        for(int i=1;i<nums.size();++i){
-            ll diff=target-prev; // Calculate the difference between target and previous element
-            prev=nums[i]-diff; // Update prev by subtracting the difference from current element
-            if(prev>target) return false; 
+        if(nums[0] > target) {
+            return false;
+        } 
+        int n = (int)nums.size();
+        ll prev = nums[0];
+        for(int i = 1; i < n; ++i){
+            ll diff = target - prev; 
+            prev = nums[i] - diff; 
+            if(prev > target) {
+                return false; 
+            }
         }
-        return true; // If all elements can be made <= target, return true
+        return true; 
     }
 
     int minimizeArrayValue(vector<int>& nums) {
-        ll left=0, right=*max_element(nums.begin(),nums.end()); // Set left to 0 and right to the maximum element in nums
-        while(left<right){ 
-            int mid=left+(right-left)/2; 
-            if(isValid(mid,nums)) right=mid; // If mid is a valid target, update right pointer
-            else left=mid+1; // If mid is not a valid target, update left pointer
+        int ans = 0;
+        ll left = 1, right = *max_element(nums.begin(), nums.end()); 
+
+        while(left <= right){ 
+            int mid = (left + right) / 2; 
+            if(isValid(mid, nums)) {
+                ans = mid;
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            } 
         }
-        return right; // Return the smallest valid target value
+
+        return ans; 
     }
 };
