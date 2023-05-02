1ea5ea039732d0978cd63c8914cd8daa2c4176ca
Baccari Ala <93733136+10Ala10@users.noreply.github.com>
2023-05-02 08:55:46 +0100

    Time: 3 ms (92.77%), Space: 10.2 MB (86.28%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
+        bool isZero = false;
         int nbneg = 0;
 
         for(auto num : nums){
             if(num == 0){
-                return 0;
+                isZero = true;
+                break;
             }
 
             if(num < 0){
                 ++nbneg;
             }
+        }
+
+        if(isZero){
+            return 0;
         }
 
         if(nbneg & 1){
